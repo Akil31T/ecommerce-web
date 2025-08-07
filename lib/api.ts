48b0df316209b678faad653ccf064573a868bd28
Akil <akil31082000@gmail.com>
2025-08-07 11:22:18 +0530

    Add the env file and save code structure

--- lib/api.ts
+++ lib/api.ts
@@ -2,10 +2,42 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:'https://ecommerce-api-nine-gilt.vercel.app/api',
+  baseURL: 'https://ecommerce-api-nine-gilt.vercel.app/api',
   headers: {
     "Content-Type": "application/json ",
   },
 });
 
-export default api;
+
+api.interceptors.response.use(
+  (response) => {
+    // Handle successful responses  
+    return response;
+  }
+  , (error) => {
+    // Handle errors globally
+    if (error.response) {
+      // The request was made and the server responded with a status code 
+      console.error("API Error:", error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error("API Error: No response received", error.request);
+    }
+    return Promise.reject(error);
+  }
+);
+
+const apiCall = async (endpoint: any, method: any, data?: any) => {
+  try {
+    const response = await api({
+      url: endpoint,
+      method: method,
+      data: data,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("API call error:", error);
+    throw error; // Re-throw the error for further handling
+  }
+};
+export default apiCall
---
+++ lib/constant.ts
@@ -0,0 +1,9 @@
+
+export const API_BASE_URL = process.env.API_BASE_URL;
+
+export const API_ENDPOINTS = { 
+    PRODUCTS: '/products',
+    CATEGORIES: '/categories',
+    ORDERS: '/orders',
+    USERS: '/users',
+} 
